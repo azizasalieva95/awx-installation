d08353b79500edb698e9b3e13fa54f8443756f0a
azaa1 <azaa.chi11@gmail.com>
2019-03-30 15:15:13 -0500

    install.sh modified

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum -y install docker-ce
 systemctl start docker
 systemctl enable docker
-cd ~/awx-installation/awx/installer/ && ansible-playbook - inventory install.yml -vv
+cd ~/awx-installation/awx/installer/ && ansible-playbook -i inventory install.yml -vv
