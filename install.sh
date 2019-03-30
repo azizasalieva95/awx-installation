cd56d4d5e82df42ab1f0db7cad7cecf30f3be9e3
azaa1 <azaa.chi11@gmail.com>
2019-03-30 15:41:09 -0500

    install.sh modified

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ yum -y install docker-ce
 systemctl start docker
 systemctl enable docker
 yum -y install docker-compose
-pip uninstall docker-py docker-compose
+pip uninstall docker-py docker-compose -y
 pip install docker-compose==1.9
 cd ~/awx-installation/awx/installer/ && ansible-playbook -i inventory install.yml -vv
